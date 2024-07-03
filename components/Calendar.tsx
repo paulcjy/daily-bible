82a70b2461587f4f2d76863a76f6653795255ff9
paulcjy <86853786+paulcjy@users.noreply.github.com>
2024-07-04 05:09:17 +0900

    fix: 오타 수정
    Date()

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -9,7 +9,7 @@ export const Calendar = () => {
 
   const exodus = [null, '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23-24', '25-26', '27-28', '29-30', '31-32', '33-34', '35-36', '37-38', '39-40']
   
-  const today = () => new Date.getDate()
+  const today = () => new Date().getDate()
 
   const setDayColor = (idx: number) => `${idx === 0 ? 'text-red-500' : ''} ${idx === 6 ? 'text-blue-600' : ''}`
 
