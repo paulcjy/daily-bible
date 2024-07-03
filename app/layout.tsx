90a9a3cf3fe6ebc49f49e6fbafd93d24d68d47da
paulcjy <paulcjy@naver.com>
2024-07-03 17:31:53 +0900

    docs: 제목 메타데이터 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "주내힘 청소년부",
+  title: "2024 여름수련회 성경읽기",
   description: "주내힘교회 청소년부 2024 여름수련회 성경읽기표",
   metadataBase: new URL('https://paulcjy.github.io/daily-bible/')
 };
