421eb66cad769ec784bafa458b48bc0a7ffb95f5
paulcjy <paulcjy@naver.com>
2024-07-07 23:17:49 +0900

    fix: opengraph-image URL 문제 해결
    - metadata에 'metadataBase'라는 속성으로 basePath를 넣었는데
    이게 썸네일 이미지를 불러올 때 링크에 basePath가 중복되도록 만들었다.
    - opengraph-image.alt.txt 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "2024 여름수련회 성경읽기",
   description: "주내힘교회 청소년부 2024 여름수련회 성경읽기표",
-  metadataBase: new URL('https://paulcjy.github.io/daily-bible/')
 };
 
 export default function RootLayout({
