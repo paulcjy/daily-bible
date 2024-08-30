c0014c5d5d6e3b31779189e76f0a93ac11db5033
paulcjy <paulcjy@naver.com>
2024-08-31 04:14:48 +0900

    fix: Modify metadata title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "2024 여름수련회 성경읽기",
-  description: "주내힘교회 청소년부 2024 여름수련회 성경읽기표",
-  metadataBase: new URL('https://paulcjy.github.io/')
-};
+  title: '주내힘교회 청소년부',
+  description: '주내힘교회 청소년부 2024 성경읽기표',
+  metadataBase: new URL('https://paulcjy.github.io/'),
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html>
       <body>{children}</body>
     </html>
-  );
+  )
 }
