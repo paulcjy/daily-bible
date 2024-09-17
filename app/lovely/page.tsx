0dc21a75e3187f5ffbf3969ed58ba2b654445026
paulcjy <paulcjy@naver.com>
2024-09-17 17:07:53 +0900

    remove: Remove unused codes

--- app/lovely/page.tsx
+++ app/lovely/page.tsx
@@ -4,13 +4,6 @@ import { LovelyCalendar } from '@/components/LovelyCalendar'
 export default function Home() {
   return (
     <div className="h-screen">
-      {/* <div className="h-[7vh] text-center align-middle">
-        <p className="sm:inline sm:text-2xl">주내힘교회 청소년부</p>
-        <p className="sm:inline sm:text-2xl sm:p-2">성경읽기표</p>
-      </div>
-      <div className="h-[6vh] text-2xl sm:text-4xl text-center align-middle">
-        7월
-      </div> */}
       <LovelyCalendar />
     </div>
   )
